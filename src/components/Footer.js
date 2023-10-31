a6ab3331a5c494ef8bac3823974f2dc124ca70a7
Nasrudeen-14 <kamal2proc@gmail.com>
2023-10-31 19:49:34 +0530

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer(){
          <div className="doormat-nav">
             <ul className='footerlink'>
                  <h4>Doormat Nav</h4>
-                 <li><Link to="/home" className="nav-item">Home</Link></li>
+                 <li><Link to="/" className="nav-item">Home</Link></li>
                  <li><Link to="/about" className="nav-item">About</Link></li>
                  <li><Link to="/menu" className="nav-item">Menu</Link></li>
                  <li><Link to="/reservation" className="nav-item">Reservations</Link></li>
